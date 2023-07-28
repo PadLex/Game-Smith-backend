e01b2bc90f8e235462f41200592c18d2f5515c97
rafaliazz <r.arfan@student.maastrichtuniversity.nl>
2023-07-28 13:59:18 +0200

    Bug fix - R

--- Recommender/src/main/java/GUI/RecommenderStarter.java
+++ Recommender/src/main/java/GUI/RecommenderStarter.java
@@ -8,9 +8,6 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import javax.swing.*;
 
 public class RecommenderStarter extends JFrame implements ActionListener {
